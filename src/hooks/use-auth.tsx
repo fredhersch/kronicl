d4cbf10eea346948ae4e11a80d7e9c5bbc449d91
Fred Hersch <fredhersch@gmail.com>
2025-08-17 07:26:03 +0000

    When I click "connect to Google photos" the same thing is happening... b

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -8,8 +8,6 @@ import {
     User as FirebaseUser, 
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword,
-    linkWithPopup,
-    unlink,
     GoogleAuthProvider,
     updateProfile,
     initializeAuth,
